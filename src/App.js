2d1772b7499a7130b056adc3ed4069c67c425b31
Collin Smith <cmsmith.tx@gmail.com>
2019-08-18 16:43:45 -0500

    added data validation and error handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Bands from './components/Bands';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
+      <h1>Get Artist Information</h1>
         <Bands />
     </div>
   );
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-//const key = `e05af0502aeaed5318fb524108b1c7df`;
 
-export class Bands extends Component {
-    
+export class Bands extends Component { 
     state = {
         value: '',
         loading: false,
         artistData: {},
         eventData: {},
-        error: ''
-    }
-    
-    componentDidMount() {
-
+        error: false
     }
 
     handleChange = (e) => {
@@ -24,48 +18,76 @@ export class Bands extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         const artist = this.state.value;
+        // if (artist.length < 1){
+        //     document.getElementById('userInput').classList.add('error');
+        //     setTimeout( () => document.getElementById('userInput').classList.remove('error'), 1000)
+        // } else {
         this.getArtistInfo(artist);
         this.getEventData(artist);
-        this.setState({ value: '', loading: true})
+        this.setState({ value: '', loading: true});
+        // }
     }
 
     getArtistInfo = (artist) => {
         const url = `https://rest.bandsintown.com/artists/${artist}?app_id=e05af0502aeaed5318fb524108b1c7df`;
         fetch(url).then(response => {
-            return response.json();
-        }).then(data => {
-            this.setState({ artistData: data })
+            if (response.ok){
+                return response.json()
+            } else {
+                throw new Error('something went wrong...')
+            }
         })
+            .then(data => {
+                this.setState({ artistData: data })
+        })
+            .catch(error => {
+                this.setState({
+                    error,
+                    loading: false,
+                })
+        });
     }
 
     getEventData = (artist) => {
         const url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=e05af0502aeaed5318fb524108b1c7df&date=upcoming`;
-        //if (this.state.artistData.upcoming_event_count > 0){
         fetch(url).then(response => {
-            return response.json();
-        }).then(data => {
-            console.log(data);
-            this.setState({eventData: data[0], loading: false})
+            if (response.ok){
+                return response.json();
+            } else {
+                throw new Error('something went wrong...')
+            }
         })
+            .then(data => {
+                this.setState({eventData: data[0], loading: false, error: false})
+        })
+            .catch(error => {
+                this.setState({
+                    error,
+                    loading: false,
+                })
+        });
     } 
-    //}
 
     render() {
         return (
-            <div>
+            <div id="artist">
                 <div>
                     <form onSubmit={this.handleSubmit}>
-                        <input type='text' onChange={this.handleChange} value={this.state.value} />
+                        <input type='text' onChange={this.handleChange} value={this.state.value} 
+                        placeholder="type an artist name..." className="p-2 rounded" id="userInput" />
+                        <input type='submit' className="btn btn-primary" />
                     </form>
                 </div>
                 <h5>{this.state.loading ? "Loading..." : ''}</h5>
+                <p>{this.state.error ? "No info found" : ''}</p>
                 <h2>{this.state.artistData.name}</h2>
-                <img src={this.state.artistData.image_url} alt={this.state.artistData.name && "photo of" + this.state.artistData.name} />
-                <p>upcoming events: {this.state.artistData.upcoming_event_count}</p>
-                <p>next show: {this.state.artistData.upcoming_event_count > 0 ? 
-                this.state.eventData.venue && this.state.eventData.venue.name + " in " 
+               {this.state.artistData && <img className="artist-img" src={this.state.artistData.image_url} alt={this.state.artistData.name && "photo of" + this.state.artistData.name} /> }
+                <p>{this.state.artistData.name && "upcoming events: " + this.state.artistData.upcoming_event_count}</p>
+                {this.state.eventData.venue && <p>{this.state.artistData.upcoming_event_count > 0 ? 
+                "next show: " + this.state.eventData.venue.name + " in " 
                 + this.state.eventData.venue.city + ", " + this.state.eventData.venue.region + " "
-                + this.state.eventData.venue.country : ''}</p>
+                + this.state.eventData.venue.country : ''}</p>}
+                {this.state.eventData.lineup && <p>{this.state.artistData.upcoming_event_count > 0 ?  "Lineup: " + this.state.eventData.lineup.join(', ') : ''}</p>}
             </div>
         )
     }
